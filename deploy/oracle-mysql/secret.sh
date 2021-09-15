316dddc4fad09ea2faabbe19ea73377971821082
churromechanic <info@churrodata.com>
2021-09-15 10:11:47 -0500

    remove unused oracle-mysql files

--- deploy/oracle-mysql/secret.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl -n testdb create secret generic  mypwds \
-        --from-literal=rootUser=root \
-        --from-literal=rootHost=% \
-        --from-literal=rootPassword="secret"
